ae3eedbbea60e9ff9ca97b6f4423e3dd8c2c230e
Haris Dzidic <haris@mop.ba>
2017-12-07 15:48:18 +0100

    Added bootstrap and buttons

--- src/components/fixtureList.js
+++ src/components/fixtureList.js
@@ -1,22 +1,35 @@
 import React, {Component} from 'react';
+import { Button, Row, Col, Panel } from 'react-bootstrap';
 
 import {connect} from 'react-redux';
 
 class FixtureList extends Component {
   renderList() {
     return this.props.fixtures.map((fixture) => {
-      return <li>{`${fixture.home} - ${fixture.away}`}</li>;
+      return <div>
+        <Row>
+          <Col xs={4} xsOffset={2}>{`${fixture.home} - ${fixture.away}`}</Col>
+          <Col xs={1}>
+            <Button bsStyle={fixture.prediction === 1 && 'Primary'} >1</Button>
+          </Col>
+          <Col xs={1}>
+            <Button bsStyle={fixture.prediction === 0 && 'Primary'}>x</Button>
+          </Col>
+          <Col xs={1}>
+            <Button bsStyle={fixture.prediction === 2 && 'Primary'}>2</Button>
+          </Col>
+        </Row>
+        <br/>
+      </div>;
     })
   }
   render() {
-    return <ul>{this.renderList()}</ul>
+    return <Panel>{this.renderList()}</Panel>
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    fixtures: state.fixtures
-  };
+function mapStateToProps({fixtures}) {
+  return {fixtures};
 }
 
 export default connect(mapStateToProps)(FixtureList)
